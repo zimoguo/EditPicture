7e41f912c77fc16549fae9c52d88a9966d49c4e7
guolili <guolili@xingshulin.com>
2016-01-15 17:58:41 +0800

    add the choose pic

--- app/src/main/java/com/zimo/guo/MainActivity.java
+++ app/src/main/java/com/zimo/guo/MainActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,11 +16,12 @@ import com.zimo.guo.activity.CameraActivity;
 import com.zimo.guo.util.PhotoUtil;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    private Button takePhoto;
+    private Button takePhoto, choosePic;
     private ImageView showPic;
 
     private String path = Environment.getExternalStorageDirectory() + File.separator + "zimo.jpg";
@@ -33,28 +36,43 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private void initView() {
         takePhoto = (Button) findViewById(R.id.take_photo);
+        choosePic = (Button) findViewById(R.id.choose_pic);
         showPic = (ImageView) findViewById(R.id.show_pic);
 
         takePhoto.setOnClickListener(this);
+        choosePic.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         Intent intent = new Intent();
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.take_photo:
                 intent.setClass(MainActivity.this, CameraActivity.class);
                 startActivityForResult(intent, 1);
+                break;
+            case R.id.choose_pic:
+                intent.setAction(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 2);
                 break;
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == 11){
-            Bitmap bitmap = BitmapFactory.decodeFile(path);
-            int degree = PhotoUtil.readPictureDegree(path);
-            showPic.setImageBitmap(PhotoUtil.rotaingImageView(degree,bitmap));
+        switch (requestCode) {
+            case 1:
+                Bitmap bitmap = BitmapFactory.decodeFile(path);
+                int degree = PhotoUtil.readPictureDegree(path);
+                showPic.setImageBitmap(PhotoUtil.rotaingImageView(degree, bitmap));
+                break;
+            case 2:
+                Uri uri = data.getData();
+                if (uri != null) {
+                    showPic.setImageURI(uri);
+                }
+                break;
         }
     }
 }
